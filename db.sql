b68f2c07f148a16327e65a43fb7e52a81d65be10
Fabio Silveira <fabio.eds@hotmail.com>
2025-11-14 14:21:19 -0800

    updatedDataBase

--- db.sql
+++ db.sql
@@ -31,7 +31,7 @@ INSERT INTO products (name, price, category, image, description) VALUES
 ('Fuel Monster', 15.00, 'sandwiches', 'https://i.ibb.co/GfQb2RCQ/gallery-1f302ebc-146e-485e-8cf3-d301deda2fae.jpg', 'Four towering layers of beef, cheese, bacon, and more—this beast is for true fuel champions only! Conquer it if you can!'),
 
 ('Coke', 3.00, 'beverages', 'https://148575793.cdn6.editmysite.com/uploads/1/4/8/5/148575793/GCJPQD3JK75ZWUJUJJAYTBAL.jpeg', '12 fl oz'),
-('Sprite', 3.00, 'beverages', 'https://i.ibb.co/W4ttKBwP/48-487814-sprite-lemon-lime-can.webp', '12 fl oz'),
+('Sprite', 3.00, 'beverages', 'https://media.istockphoto.com/id/458556265/photo/sprite-can-on-an-isolated-white-background.jpg?s=170667a&w=0&k=20&c=x0KJntljvTeAIEdubKpsYQnE2-I91k0cFljM2MUyaZk=', '12 fl oz'),
 ('Dr. Pepper', 3.00, 'beverages', 'https://souvlakigeorge.com/wp-content/uploads/2023/10/drink-drpepper.jpg', '12 fl oz'),
 ('Fanta Orange', 3.00, 'beverages', 'https://i5.walmartimages.com/seo/Fanta-Orange-Soda-7-5oz-Small-Mini-Cans-3-8-Packs-24-Cans_26c52f08-addf-4c9c-a697-e0616f70aa55.333b41038a44fe079e80978a46f78a7c.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', '12 fl oz'),
 ('Diet Coke', 3.00, 'beverages', 'https://boxncase.com/cdn/shop/files/bevcc18.gallery_4e2b9308-e410-48c7-bc13-913760bc1311.jpg?v=1691529304', '12 fl oz'),
@@ -40,7 +40,7 @@ INSERT INTO products (name, price, category, image, description) VALUES
 ('French Fries', 4.00, 'sides', 'https://png.pngtree.com/png-clipart/20240901/original/pngtree-delicious-french-fries-fast-food-png-image_15902895.png', 'Golden, crispy, and perfectly salted. The ultimate sidekick for any sandwich or burger!'),
 ('Onion Rings', 4.00, 'sides', 'https://media.istockphoto.com/id/518802156/photo/onion-rings.jpg?s=612x612&w=0&k=20&c=SVVifA9O-1cjBgy9bI-YAhcAAI9ohTv34VmOxhFIMpU=', 'Thick-cut onions coated in crispy batter and fried to perfection. Crunchy and crave-worthy!'),
 ('Salad', 4.00, 'sides', 'https://media.istockphoto.com/id/1090421790/photo/healthy-salad-caesar-in-plastic-package-for-take-away-or-food-delivery-isolated-on-a-white.jpg?s=612x612&w=0&k=20&c=IpDYQJ8IJ1A58TLFCuS16bIXwOtu9VGxAtFNCruRh8E=', 'A light and fresh mix of lettuce, tomatoes, and veggies. Perfect for a lighter fuel option.'),
-('Mozzarella Sticks', 4.00, 'sides', 'https://i.ibb.co/5XJ1tJJ1/yummy-delicious-mozzarella-sticks-isolated-white-background-generative-ai-362882027.jpg', 'Crispy on the outside, melty on the inside. Served hot with marinara dipping sauce for extra flavor punch!'),
+('Mozzarella Sticks', 4.00, 'sides', 'https://thumbs.dreamstime.com/b/yummy-delicious-mozzarella-sticks-isolated-white-background-yummy-delicious-mozzarella-sticks-isolated-white-363106962.jpg', 'Crispy on the outside, melty on the inside. Served hot with marinara dipping sauce for extra flavor punch!'),
 
 ('Chocolate Milkshake', 5.00, 'desserts', 'https://uproxx.com/wp-content/uploads/2022/08/menuproduct-Hot_Fudge_Classic_Shake.webp', 'Rich, creamy, and full of chocolatey goodness. Blended to perfection for the ultimate sweet refreshment.'),
 ('Strawberry Sundae', 3.00, 'desserts', 'https://pinoycupidgifts.com/wp-content/uploads/2024/05/strawberry-sundae.jpg', 'Cool vanilla ice cream topped with sweet strawberry syrup and chunks of real strawberries. A fruity finish for your meal!'),
